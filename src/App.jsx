151f5f4e772103538697800b1774ac8a3463c5b2
unknown <moshawa0@gmail.com>
2022-07-05 00:45:53 -0400

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Homepage from './pages/Homepage'
 import SetupPage from './pages/SetupPage'
-
+import { Route, Routes, Link } from 'react-router-dom'
 
 
 function App() {
@@ -18,13 +18,20 @@ function App() {
 
   return (
     <div className="App h-screen w-screen flex items-center justify-center p-2">
-      {initialSetup
+      {/* {initialSetup
         ?
         <Homepage options={options} />
         :
         <SetupPage setInitialSetup={setInitialSetup} />
-      }
+      } */}
+      <Link to='/app'>go home</Link>
+      <Link to='/setup'>go setup</Link>
       {/* <Homepage options={options} /> */}
+      <Routes>
+        <Route path='/app' element={<Homepage />} />
+        <Route path='/setup' element={<SetupPage />} />
+
+      </Routes>
     </div>
   )
 }
--- src/pages/SetupPage.jsx
+++ src/pages/SetupPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default function SetupPage(props) {
+
     return (
         <div>
             <h1 className='text-7xl -bold m-10'>Let's get you set up...</h1>
-            {/* <button className='btn-primary' onClick={() => props.setInitialSetup(true)}>Set Setup</button> */}
+            <button className='btn-primary' onClick={() => props.setInitialSetup(true)}>Set Setup</button>
         </div>
     )
 }
